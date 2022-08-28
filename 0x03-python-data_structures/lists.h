7a13fb156f7510e87ce1b4933258e65de771c73a
Alphydoo <aaoshiole@yahoo.com>
2022-08-28 12:45:58 +0100

    Header File

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -18,6 +18,10 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
 def print_list_integer(my_list=[]);
 def element_at(my_list, idx);
 def replace_in_list(my_list, idx, element);
@@ -32,5 +36,7 @@ def divisible_by_2(my_list=[]);
 def delete_at(my_list=[], idx=0);
 int is_palindrome(listint_t **head);
 void print_python_list_info(PyObject *p);
+
+int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */

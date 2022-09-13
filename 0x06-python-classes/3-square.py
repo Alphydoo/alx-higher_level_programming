8e72fa4b381290331934be1c788ecb14504e4ea1
Alphydoo <aaoshiole@yahoo.com>
2022-09-13 13:16:59 +0100

    3. Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+"""
+Super simple Square Module.
+"""
+
+
 class Square:
+    """
+    Only does one thing: Gives you its area.
+    """
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError('size must be an integer')
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
     def area(self):
-        size = self.__size
-        return size * size
+        return (self.__size ** 2)

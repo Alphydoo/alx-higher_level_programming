3fa74b2d8de8a94b096a489b2d22e2583e99abe9
Alphydoo <aaoshiole@yahoo.com>
2022-09-26 16:18:16 +0100

    13. Can I?

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
-def inherits_from(obj, a_class):
-    return (0)
-print(inherits_from(obj, a_class))
+"""Function add_attribute
+"""
+
+
+def add_attribute(a_class, name, value):
+    """Adds new attribute to an object if it's possible
+    """
+
+    # Set for O(1) membership test
+    cannot_add = {int, str, float, list, dict, tuple, frozenset, type, object}
+
+    if type(a_class) in cannot_add:
+        raise TypeError("can't add new attribute")
+
+    a_class.__setattr__(name, value)

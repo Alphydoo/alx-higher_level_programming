09060865638fe42627b85e7f4b351927770c62d4
Alphydoo <aaoshiole@yahoo.com>
2022-09-26 16:16:45 +0100

    12. My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
-def is_kind_of_class(obj, a_class):
-    return (0)
-print(is_kind_of_class(obj, a_class))
+"""MyInt inherits from int
+"""
+
+
+class MyInt(int):
+    """Inherits from int
+    """
+
+    def __eq__(self, value):
+        """Magic method equals
+        """
+
+        return super().__ne__(value)
+
+    def __ne__(self, value):
+        """Magic method not equals
+        """
+
+        return super().__eq__(value)

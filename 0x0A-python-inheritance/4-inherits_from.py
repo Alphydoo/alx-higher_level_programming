b59ee1ecd3a3ecedc3a4dc71be136127ce1e68fa
Alphydoo <aaoshiole@yahoo.com>
2022-09-26 16:05:45 +0100

    4. Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-void print_python_int(PyObject *p):
-    return (0)
-print(void print_python_int(PyObject *p))
+"""inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """Function that returns True if the object is an instance
+    of a class that inherited (directly or indirectly) from the
+    specified class; otherwise False.
+    """
+
+    if type(obj) is a_class or not isinstance(obj, a_class):
+        return False
+    else:
+        return True

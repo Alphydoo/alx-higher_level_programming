8640e8d46aeccfdb0a3b16abcd9e4651b981d71a
Alphydoo <aaoshiole@yahoo.com>
2022-09-26 16:15:26 +0100

    11. Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python3
-def is_same_class(obj, a_class):
-    return (0)
-print(is_same_class(obj, a_class))
+"""Square inherits from Rectangle which inherits from BaseGeometry
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square data that inherits from Rectangle
+       init `size` twice because width and height are same in squares
+    """
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Method to calculate area of the square
+        """
+
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Magic method to print square description
+        """
+
+        return "[Square] {}/{}".format(self.__size, self.__size)

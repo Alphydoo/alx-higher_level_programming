1b388af5b304e236e8c9f6d72a7af2d9850e65d7
Alphydoo <aaoshiole@yahoo.com>
2022-09-28 16:25:34 +0100

    10. Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-"""Student
+""" Module that defines the class Student
 """
 
 
 class Student:
-    """Contains student data
-    """
+    """ Class to create student instances """
 
     def __init__(self, first_name, last_name, age):
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Retrieves dictionary of Student
-        """
+    def to_json(self, attrs=None):
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
 
-        return self.
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj

7797e7371269da5615d55e0860b9519939a6cc1c
Alphydoo <aaoshiole@yahoo.com>
2022-11-01 16:36:01 +0100

    20. Temperatures #2

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,3 @@
-#!/usr/bin/env bash
-#DESCRIPTION OF FUNCTIONALITY
+-- displays the max temperature of each state
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
+

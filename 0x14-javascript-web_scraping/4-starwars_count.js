3c075f39a83b361e0292d5dd696afe4ffd4eb7b9
Alphydoo <aaoshiole@yahoo.com>
2023-01-17 17:47:32 +0100

    4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,24 @@
 #!/usr/bin/node
-// JS Script
-require('request').get(process.argv[2], function (err, r, body) {
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else {
-    let counter = 0;
-    let data = JSON.parse(body).results;
-    for (let i = 0; i < data.length; i++) {
-      for (let n = 0; n < data[i].characters.length; n++) {
-        if (data[i].characters[n].includes('/18/')) {
-          counter++;
-          break;
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
         }
       }
     }
-    console.log(counter);
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });

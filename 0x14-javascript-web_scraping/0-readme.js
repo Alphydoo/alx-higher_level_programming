95fede82e29269469581fe6ed25163cffbf9b713
Alphydoo <aaoshiole@yahoo.com>
2023-01-17 17:40:15 +0100

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-// JS Script
-let fs = require('fs');
-fs.readFile(process.argv[2], function (err, contents) {
-  if (!err) {
-    console.log(contents.toString().trim());
-  } else {
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
     console.log(err);
+  } else {
+    console.log(data);
   }
 });

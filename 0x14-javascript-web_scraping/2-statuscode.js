70997c647273d05b72ef6cd8dd258794efdb6354
Alphydoo <aaoshiole@yahoo.com>
2023-01-23 10:06:03 +0100

    2. Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-
+const argv = process.argv;
+const url = argv[2];
 const request = require('request');
-const URL = process.argv[2];
-
-request(URL, function (err, response) {
-  if (err) {
-    console.log(err);
+request(url, function (error, response) {
+  if (error) {
+    console.error('error:', error);
   } else {
-    console.log('code: ' + response.statusCode);
+    console.log(`code: ${response.statusCode}`);
   }
 });

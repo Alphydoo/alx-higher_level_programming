8e15d068dac4df77035426849bb368f3d819badd
Alphydoo <aaoshiole@yahoo.com>
2023-01-23 10:04:14 +0100

    1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
+const argv = process.argv;
 const fs = require('fs');
-const file = process.argv[2];
-const string = process.argv[3];
-
-fs.writeFile(file, string, 'utf-8', function (err) {
-  if (err) console.log(err);
+const fileName = argv[2];
+const string = argv[3];
+fs.writeFile(fileName, string, 'utf-8', function (err) {
+  if (err) return console.log(err);
 });
